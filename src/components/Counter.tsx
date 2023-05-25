b2ab6e893499c62d6656896f51b5e06c94fe6b2b
vitaliyis <vetal.friend@gmail.com>
2023-05-25 08:31:32 +0300

    initial commit

---
+++ src/components/Counter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Counter.module.scss';
+
+const Counter = () => {
+    return (
+        <div className={style.color}>
+            Counter
+        </div>
+    );
+};
+
+export default Counter;
