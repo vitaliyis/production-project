2ee7318c13c03b7429f905f10b56a9b096320ded
vitaliyis <vetal.friend@gmail.com>
2023-05-29 11:34:59 +0300

    add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,31 @@ import {BuildOptions} from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     const cssLoader = {
             test: /\.s[ac]ss$/i,
             use: [
@@ -33,7 +58,20 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: 'ts-loader',
         exclude: /node_modules/,
     }
+
+    const fileLoader = {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                },
+            ],
+        }
+
     return [
+        fileLoader,
+        svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader
     ]
---
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {useTranslation} from 'react-i18next';
+
+const AboutPage = () => {
+    const { t } = useTranslation('about');
+
+    return (
+        <div>
+            {t('About page')}
+        </div>
+    );
+};
+
+export default AboutPage;
